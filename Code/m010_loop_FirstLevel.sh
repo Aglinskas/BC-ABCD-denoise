0f5b7e9989af7002bfff750b738e25546aa3bb84
Aglinskas <aglinska@l001.cm.cluster>
2024-10-25 17:56:26 -0400

    working but linear?

--- Code/m010_loop_FirstLevel.sh
+++ Code/m010_loop_FirstLevel.sh
@@ -4,7 +4,7 @@
 #SBATCH --error=../Data/SLURM-reports/MATLAB-1stLevel/MATLAB-1stLevel-errors-_%a.txt
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --time=01:00:00
+#SBATCH --time=03:00:00
 #SBATCH --mem=64gb
 #SBATCH --array=0-34
 
@@ -16,6 +16,8 @@ while IFS= read -r line; do # # Read the file line by line and add each line (fi
     subject_list+=("$line")
 done < subject_list.txt
 
+date
+echo date
 echo Found ${#subject_list[@]} subjects
 echo subNum is ${SLURM_ARRAY_TASK_ID}
 echo subID is ${subject_list[${SLURM_ARRAY_TASK_ID}]}
@@ -24,63 +26,70 @@ subID=${subject_list[${SLURM_ARRAY_TASK_ID}]}
 module load matlab
 
 
-for rID in $(seq 1 2);
+prefixs=(preproc COMPCORR DEEPCOR-TG-1 DEEPCOR-BG-1 DEEPCOR-FG-1)
+
+for prefix in ${prefixs[@]}
 do
-
-    # epi_path=/mmfs1/data/aglinska/BC-ABCD-denoise/Data/020-fmriprepped/${subID}/ses-baselineYear1Arm1/func/s8_${subID}_ses-baselineYear1Arm1_task-nback_run-0${rID}_space-MNI152NLin2009cAsym_res-2_desc-preproc_bold.nii
-    # analysis_dir=../analyses_SPM/s8_preproc_${rID}/${subID}
+    for rID in $(seq 1 2);
+    do
     
-    # epi_path=/mmfs1/data/aglinska/BC-ABCD-denoise/Data/020-fmriprepped/${subID}/ses-baselineYear1Arm1/func/${subID}_ses-baselineYear1Arm1_task-nback_run-0${rID}_space-MNI152NLin2009cAsym_res-2_desc-COMPCORR_bold.nii
-    # analysis_dir=../analyses_SPM/COMPCORR_${rID}/${subID}
+        epi_path=/mmfs1/data/aglinska/BC-ABCD-denoise/Data/020-fmriprepped/${subID}/ses-baselineYear1Arm1/func/${subID}_ses-baselineYear1Arm1_task-nback_run-0${rID}_space-MNI152NLin2009cAsym_res-2_desc-${prefix}_bold.nii
+        analysis_dir=../analyses_SPM/${prefix}_${rID}/${subID}
+            
+        multicond_path=/mmfs1/data/aglinska/BC-ABCD-denoise/Data/030-subject-multiconds/${subID}_nback_multicond_run_${rID}.mat
+        
+        echo ${epi_path}
+        echo ${analysis_dir}
+        echo ${multicond_path}
+        
     
-    # epi_path=/mmfs1/data/aglinska/BC-ABCD-denoise/Data/020-fmriprepped/${subID}/ses-baselineYear1Arm1/func/${subID}_ses-baselineYear1Arm1_task-nback_run-0${rID}_space-MNI152NLin2009cAsym_res-2_desc-DEEPCOR_bold.nii
-    # analysis_dir=../analyses_SPM/DEEPCOR_strict_${rID}/${subID}
-
-    # epi_path=/mmfs1/data/aglinska/BC-ABCD-denoise/Data/020-fmriprepped/${subID}/ses-baselineYear1Arm1/func/${subID}_ses-baselineYear1Arm1_task-nback_run-0${rID}_space-MNI152NLin2009cAsym_res-2_desc-DEEPCOR_bold.nii
-    # analysis_dir=../analyses_SPM/DEEPCOR_${rID}/${subID}
-
-
-
-    # epi_path=/mmfs1/data/aglinska/BC-ABCD-denoise/Data/020-fmriprepped/${subID}/ses-baselineYear1Arm1/func/${subID}_ses-baselineYear1Arm1_task-nback_run-0${rID}_space-MNI152NLin2009cAsym_res-2_desc-DEEPCOR-TG_bold.nii
-    # analysis_dir=../analyses_SPM/DEEPCOR_TG_${rID}/${subID}
-
-    # epi_path=/mmfs1/data/aglinska/BC-ABCD-denoise/Data/020-fmriprepped/${subID}/ses-baselineYear1Arm1/func/${subID}_ses-baselineYear1Arm1_task-nback_run-0${rID}_space-MNI152NLin2009cAsym_res-2_desc-DEEPCOR-FG_bold.nii
-    # analysis_dir=../analyses_SPM/DEEPCOR_FG_${rID}/${subID}
+        if [ -e $epi_path ]; then
+            echo "EPI path exists"
+        else 
+            echo "!!!! EPI PATH DOES NOT EXIST !!!!"
+            exit 1
+        fi 
+        
+        if [ -e $multicond_path ]; then
+            echo "multicond_path path exists"
+        else 
+            echo "!!!! multicond_path PATH DOES NOT EXIST !!!!"
+            exit 1
+        fi 
     
-    # epi_path=/mmfs1/data/aglinska/BC-ABCD-denoise/Data/020-fmriprepped/${subID}/ses-baselineYear1Arm1/func/${subID}_ses-baselineYear1Arm1_task-nback_run-0${rID}_space-MNI152NLin2009cAsym_res-2_desc-DEEPCOR-twin_bold.nii
-    # analysis_dir=../analyses_SPM/DEEPCOR_twin_${rID}/${subID}
-
-    epi_path=/mmfs1/data/aglinska/BC-ABCD-denoise/Data/020-fmriprepped/${subID}/ses-baselineYear1Arm1/func/${subID}_ses-baselineYear1Arm1_task-nback_run-0${rID}_space-MNI152NLin2009cAsym_res-2_desc-DEEPCOR-BG_bold.nii
-    analysis_dir=../analyses_SPM/DEEPCOR_BG_${rID}/${subID}
-
+        mkdir -p $analysis_dir
+        
+        matlab -nodisplay -r "m010_func_fist_level_v2('${analysis_dir}','${epi_path}','${multicond_path}');exit"
     
-    
-    
-    multicond_path=/mmfs1/data/aglinska/BC-ABCD-denoise/Data/030-subject-multiconds/${subID}_nback_multicond_run_${rID}.mat
-    
-    echo ${epi_path}
-    echo ${analysis_dir}
-    echo ${multicond_path}
-    
-
-    if [ -e $epi_path ]; then
-        echo "EPI path exists"
-    else 
-        echo "!!!! EPI PATH DOES NOT EXIST !!!!"
-        exit 1
-    fi 
-    
-    if [ -e $multicond_path ]; then
-        echo "multicond_path path exists"
-    else 
-        echo "!!!! multicond_path PATH DOES NOT EXIST !!!!"
-        exit 1
-    fi 
-
-    mkdir -p $analysis_dir
-    
-    matlab -nodisplay -r "m010_func_fist_level_v2('${analysis_dir}','${epi_path}','${multicond_path}');exit"
-
+    done
 done
 
+
+
+
+
+    # # epi_path=/mmfs1/data/aglinska/BC-ABCD-denoise/Data/020-fmriprepped/${subID}/ses-baselineYear1Arm1/func/${subID}_ses-baselineYear1Arm1_task-nback_run-0${rID}_space-MNI152NLin2009cAsym_res-2_desc-COMPCORR_bold.nii
+    # # analysis_dir=../analyses_SPM/COMPCORR_${rID}/${subID}
+    
+    # # epi_path=/mmfs1/data/aglinska/BC-ABCD-denoise/Data/020-fmriprepped/${subID}/ses-baselineYear1Arm1/func/${subID}_ses-baselineYear1Arm1_task-nback_run-0${rID}_space-MNI152NLin2009cAsym_res-2_desc-DEEPCOR_bold.nii
+    # # analysis_dir=../analyses_SPM/DEEPCOR_strict_${rID}/${subID}
+
+    # # epi_path=/mmfs1/data/aglinska/BC-ABCD-denoise/Data/020-fmriprepped/${subID}/ses-baselineYear1Arm1/func/${subID}_ses-baselineYear1Arm1_task-nback_run-0${rID}_space-MNI152NLin2009cAsym_res-2_desc-DEEPCOR_bold.nii
+    # # analysis_dir=../analyses_SPM/DEEPCOR_${rID}/${subID}
+
+
+    # # epi_path=/mmfs1/data/aglinska/BC-ABCD-denoise/Data/020-fmriprepped/${subID}/ses-baselineYear1Arm1/func/${subID}_ses-baselineYear1Arm1_task-nback_run-0${rID}_space-MNI152NLin2009cAsym_res-2_desc-DEEPCOR-TG_bold.nii
+    # # analysis_dir=../analyses_SPM/DEEPCOR_TG_${rID}/${subID}
+
+    # # epi_path=/mmfs1/data/aglinska/BC-ABCD-denoise/Data/020-fmriprepped/${subID}/ses-baselineYear1Arm1/func/${subID}_ses-baselineYear1Arm1_task-nback_run-0${rID}_space-MNI152NLin2009cAsym_res-2_desc-DEEPCOR-FG_bold.nii
+    # # analysis_dir=../analyses_SPM/DEEPCOR_FG_${rID}/${subID}
+    
+    # # epi_path=/mmfs1/data/aglinska/BC-ABCD-denoise/Data/020-fmriprepped/${subID}/ses-baselineYear1Arm1/func/${subID}_ses-baselineYear1Arm1_task-nback_run-0${rID}_space-MNI152NLin2009cAsym_res-2_desc-DEEPCOR-twin_bold.nii
+    # # analysis_dir=../analyses_SPM/DEEPCOR_twin_${rID}/${subID}
+
+    # epi_path=/mmfs1/data/aglinska/BC-ABCD-denoise/Data/020-fmriprepped/${subID}/ses-baselineYear1Arm1/func/${subID}_ses-baselineYear1Arm1_task-nback_run-0${rID}_space-MNI152NLin2009cAsym_res-2_desc-DEEPCOR-BG_bold.nii
+    # analysis_dir=../analyses_SPM/DEEPCOR_BG_${rID}/${subID}
+
+
+  
 
--- Code/slurm-02-pp-run-deepcor.sh
+++ Code/slurm-02-pp-run-deepcor.sh
@@ -5,42 +5,38 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --gpus-per-node=1
-#SBATCH --time=05:00:00
+#SBATCH --time=06:00:00
 #SBATCH --mem=64gb
 #SBATCH --partition=gpua100
-#SBATCH --array=25-34
+#SBATCH --array=0-34
 
 
-notebook_name='072-DeepCorr-gamma.ipynb'
+notebook_name='075-DeepCorr-gamma.ipynb'
+outdir=../Data/040-DeepCor-Papermill/pp-notebooks-v2_1_0--linear-200-epochs
+analysis_name='linear-200-epochs'
 
-outname=../Data/040-DeepCor-Papermill/pp-notebooks/072-DeepCorr-gamma-S${SLURM_ARRAY_TASK_ID}-R1.ipynb
-echo $notebook_name
-papermill $notebook_name $outname --autosave-cell-every 5 --progress-bar -p s ${SLURM_ARRAY_TASK_ID} -p r 1
+mkdir -p ${outdir}
+for rep in 0 1 2 3 4
+do
 
-outname=../Data/040-DeepCor-Papermill/pp-notebooks/072-DeepCorr-gamma-S${SLURM_ARRAY_TASK_ID}-R2.ipynb
-echo $notebook_name
-papermill $notebook_name $outname --autosave-cell-every 5 --progress-bar -p s ${SLURM_ARRAY_TASK_ID} -p r 2
+    outname=${outdir}/075-DeepCorr-gamma-S${SLURM_ARRAY_TASK_ID}-R1-rep-$rep.ipynb
+    echo $outname
+    papermill $notebook_name $outname --autosave-cell-every 5 --progress-bar -p s ${SLURM_ARRAY_TASK_ID} -p r 1 -p rep $rep -p analysis_name $analysis_name
+    
+    outname=${outdir}/075-DeepCorr-gamma-S${SLURM_ARRAY_TASK_ID}-R2-$rep.ipynb
+    echo $notebook_name
+    papermill $notebook_name $outname --autosave-cell-every 5 --progress-bar -p s ${SLURM_ARRAY_TASK_ID} -p r 2 -p rep $rep -p analysis_name $analysis_name
+    
+done
+
+# outname=../Data/040-DeepCor-Papermill/pp-notebooks/072-DeepCorr-gamma-S${SLURM_ARRAY_TASK_ID}-R1.ipynb
+# echo $notebook_name
+# papermill $notebook_name $outname --autosave-cell-every 5 --progress-bar -p s ${SLURM_ARRAY_TASK_ID} -p r 1
+
+# outname=../Data/040-DeepCor-Papermill/pp-notebooks/072-DeepCorr-gamma-S${SLURM_ARRAY_TASK_ID}-R2.ipynb
+# echo $notebook_name
+# papermill $notebook_name $outname --autosave-cell-every 5 --progress-bar -p s ${SLURM_ARRAY_TASK_ID} -p r 2
 
 
-# echo $0
 
-# bash
 
-# source .bash_profile
-
-# # >>> conda initialize >>>
-# # !! Contents within this block are managed by 'conda init' !!
-# __conda_setup="$('/data/aglinska/anaconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
-# if [ $? -eq 0 ]; then
-#     eval "$__conda_setup"
-# else
-#     if [ -f "/data/aglinska/anaconda3/etc/profile.d/conda.sh" ]; then
-#         . "/data/aglinska/anaconda3/etc/profile.d/conda.sh"
-#     else
-#         export PATH="/data/aglinska/anaconda3/bin:$PATH"
-#     fi
-# fi
-# unset __conda_setup
-# # <<< conda initialize <<<
-
-# conda activate tf231
